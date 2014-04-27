6f09b30f6920f1a88d15b299aa4d3c8fe4ea2c2e
JanaC <jana_at_az@yahoo.com>
2014-04-27 11:39:02 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,13 +1,12 @@
-
 # Usage example: 
-# call makeMatrix and cacheinverese once they are placed in the working
+# call makeMatrix and cachesolve once they are placed in the working
 # environment.
 # To Call: 
 # Create a matrix, p.e Variable1 is a non-singular square matrix
 # >dummyVar1<-makeMatrix(Variable1)
-# >dummyVar2<-cacheinverse(dummyVar1)
+# >dummyVar2<-cachesolve(dummyVar1)
 # >print (dummyVar2)
-# any time the cacheinverse(dummyVar2) is called after the first time, 
+# any time the cachesolve(dummyVar2) is called after the first time, 
 # the function will retrieve the value stored in memory instead of 
 # recalculating the inverse of Variable1
 
@@ -30,11 +29,11 @@ makeMatrix<-function (mtx=matrix()){
 # the function calculates the inverse depending on the contents of the lst created by
 # makeMatrix
 # if the element of the list called "setinverse" is not null (menaing it contains the 
-# result of calculating the inverse using "solve", the "cacheinverse" function knows that
+# result of calculating the inverse using "solve", the "cachesolve" function knows that
 # the calculation has been performed and just retrieves the info stored in that element of
 # the list, otherwise it calculates the inverse caling the function "solve"
 #
-cacheinverse<-function (mtx, ...){
+cachesolve<-function (mtx, ...){
       inv<-mtx$getinverse()
       if(!is.null(inv)){
             message("getting cache data")
