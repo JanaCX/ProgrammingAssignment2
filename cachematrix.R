7b0a40a831a88ea9ed838f2f9a0c86e3f192af5e
JanaC <jana_at_az@yahoo.com>
2014-04-27 11:35:38 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+# Usage example: 
+# call makeMatrix and cacheinverese once they are placed in the working
+# environment.
+# To Call: 
+# Create a matrix, p.e Variable1 is a non-singular square matrix
+# >dummyVar1<-makeMatrix(Variable1)
+# >dummyVar2<-cacheinverse(dummyVar1)
+# >print (dummyVar2)
+# any time the cacheinverse(dummyVar2) is called after the first time, 
+# the function will retrieve the value stored in memory instead of 
+# recalculating the inverse of Variable1
 
-makeCacheMatrix <- function(x = matrix()) {
+# This first function creates the "decision list", when the second function runs 
+# the elements in this list will tell whether the inverse has been calculated or not
 
+makeMatrix<-function (mtx=matrix()){
+      inv<-NULL
+      set<-function (y){
+            mtx<<-y
+            inv<<-NULL
+      }
+      get<-function() mtx
+      setinverse<-function(inverse) inv<<-inverse
+      getinverse<-function() inv
+      list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#
+# checking for the inverse matrix in memory / calculating the inverse 
+# the function calculates the inverse depending on the contents of the lst created by
+# makeMatrix
+# if the element of the list called "setinverse" is not null (menaing it contains the 
+# result of calculating the inverse using "solve", the "cacheinverse" function knows that
+# the calculation has been performed and just retrieves the info stored in that element of
+# the list, otherwise it calculates the inverse caling the function "solve"
+#
+cacheinverse<-function (mtx, ...){
+      inv<-mtx$getinverse()
+      if(!is.null(inv)){
+            message("getting cache data")
+            return (inv)
+      }
+      data<-mtx$get()
+      inv<-solve(data, ...)
+      mtx$setinverse(inv)
+      inv
 }
